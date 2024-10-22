86795c566f3668e1f4be988466623bd3ef409b9b
nbkhope <ryumemaru@yahoo.com>
2024-10-22 16:24:31 -0400

    Add Cancel button next to submit one

--- frontend/src/views/UserSecretsPage/components/UserSecretsForm.tsx
+++ frontend/src/views/UserSecretsPage/components/UserSecretsForm.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 
 import { Button, FormControl, Input } from "@app/components/v2";
-// import { useCreateConsumerSecret } from "@app/hooks/api/consumerSecrets/mutations";
+// import { useCreateConsumerSecret } from "@app/hooks/api/consumerSecrets";
 
 export const UserSecretsForm = (props) => {
   const { handleSubmit, register } = useForm();
@@ -9,8 +9,11 @@ export const UserSecretsForm = (props) => {
   // const { mutateAsync: createConsumerSecret } = useCreateConsumerSecret();
 
   return (
-    <form onSubmit={handleSubmit(props.onSubmit)}>
-      <FormControl label="Name">
+    <form
+      onSubmit={handleSubmit(props.onSubmit)}
+      // noValidate
+    >
+      <FormControl isRequired label="Name">
         <Input {...register('name')} />
       </FormControl>
       <FormControl label="Username">
@@ -19,8 +22,16 @@ export const UserSecretsForm = (props) => {
       <FormControl label="Password">
         <Input {...register('password')} type="password" />
       </FormControl>
-      <div>
-        <Button type="submit">Create</Button>
+      <div class="mt-7">
+        <Button type="submit" className="mr-4">Create</Button>
+        <Button
+          // key="layout-cancel-create-project"
+          onClick={props.onClose}
+          variant="plain"
+          colorSchema="secondary"
+        >
+          Cancel
+        </Button>
       </div>
     </form>
   )
