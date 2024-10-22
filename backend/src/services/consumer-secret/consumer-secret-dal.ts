ef961997bd2a977fc5f7c2a22ad281538fb08c17
nbkhope <ryumemaru@yahoo.com>
2024-10-22 16:21:33 -0400

    Update consumerSecretDALFactory to provide basic CRUD operations using ormify

--- backend/src/services/consumer-secret/consumer-secret-dal.ts
+++ backend/src/services/consumer-secret/consumer-secret-dal.ts
@@ -1,9 +1,9 @@
 import { TDbClient } from "@app/db";
 import { TableName } from "@app/db/schemas";
+import { ormify } from "@app/lib/knex";
 
 export type TConsumerSecretDALFactory = ReturnType<typeof consumerSecretDALFactory>;
 
 export const consumerSecretDALFactory = (db: TDbClient) => {
-
-  return {  };
+  return ormify(db, TableName.ConsumerSecret);
 };
