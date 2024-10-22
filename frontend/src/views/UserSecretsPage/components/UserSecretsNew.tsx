a50aa3b42247880a0a484eb42ed922da2c1084f4
nbkhope <ryumemaru@yahoo.com>
2024-10-22 16:24:09 -0400

    Move user secrets form to a modal

--- frontend/src/views/UserSecretsPage/components/UserSecretsNew.tsx
+++ frontend/src/views/UserSecretsPage/components/UserSecretsNew.tsx
@@ -1,13 +1,39 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCreateConsumerSecret } from "@app/hooks/api/consumerSecrets"
+import { usePopUp } from "@app/hooks";
 import { UserSecretsForm } from "./UserSecretsForm"
+import {
+  Button,
+  Modal,
+  ModalContent
+} from "@app/components/v2";
+
+const POPUP_USER_SECRET_CREATE = "createUserSecret";
 
 export const UserSecretsNew = () => {
+  const { mutateAsync: createConsumerSecret } = useCreateConsumerSecret();
+  const { handlePopUpClose, handlePopUpOpen, handlePopUpToggle, popUp } = usePopUp([POPUP_USER_SECRET_CREATE]);
+
   function onSubmit(formFields) {
     console.log({formFields})
+
+    createConsumerSecret(formFields);
   }
 
   return (
-    <div>
-      <UserSecretsForm onSubmit={onSubmit} />
-    </div>
+    <>
+      <div className="flex justify-between mb-4">
+        <div>
+          {/* TODO: search */}
+        </div>
+        <Button leftIcon={<FontAwesomeIcon icon={faPlus} />}onClick={() => handlePopUpOpen(POPUP_USER_SECRET_CREATE)}>Create User Secret</Button>
+      </div>
+      <Modal isOpen={popUp[POPUP_USER_SECRET_CREATE].isOpen} onOpenChange={(isOpen) => handlePopUpToggle(POPUP_USER_SECRET_CREATE, isOpen)}>
+        <ModalContent title="Title" subTitle="Subtitle">
+          <UserSecretsForm onClose={() => handlePopUpClose(POPUP_USER_SECRET_CREATE)} onSubmit={onSubmit} />
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
