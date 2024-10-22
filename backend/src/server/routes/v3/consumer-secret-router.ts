24a4ccfabbab3dd104efa7abba1ac5fe121211ad
nbkhope <ryumemaru@yahoo.com>
2024-10-22 16:22:59 -0400

    Enable CREATE and DELETE for consumer secret

--- backend/src/server/routes/v3/consumer-secret-router.ts
+++ backend/src/server/routes/v3/consumer-secret-router.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 import { AuthMode } from "@app/services/auth/auth-type";
 import { ConsumerSecretsSchema } from "@app/db/schemas";
+import { NotFoundError } from "@app/lib/errors";
 import { readLimit } from "@app/server/config/rateLimiter"; 
 import { verifyAuth } from "@app/server/plugins/auth/verify-auth";
 
@@ -65,33 +66,38 @@ export const registerConsumerSecretRouter = async (server: FastifyZodProvider) =
   //   },
   // })
 
-  // server.route({
-  //   method: "POST",
-  //   url: "/",
-  //   config: {
-  //     rateLimit: readLimit,
-  //   },
-  //   schema: {
-  //     description: "Create consumer secrets",
-  //     security: [
-  //       {
-  //         bearerAuth: []
-  //       }
-  //     ],
-  //     response: {
-  //       200: ConsumerSecretsSchema
-  //     }
-  //   },
-  //   onRequest: verifyAuth([
-  //     AuthMode.JWT,
-  //     AuthMode.API_KEY,
-  //     AuthMode.SERVICE_TOKEN,
-  //     AuthMode.IDENTITY_ACCESS_TOKEN
-  //   ]),
-  //   handler: async (req) => {
-  //     return server.services.consumerSecret.create();
-  //   },
-  // });
+  server.route({
+    method: "POST",
+    url: "/",
+    config: {
+      rateLimit: readLimit,
+    },
+    schema: {
+      description: "Create consumer secrets",
+      security: [
+        {
+          bearerAuth: []
+        }
+      ],
+      response: {
+        200: ConsumerSecretsSchema
+      },
+      body: ConsumerSecretsSchema.omit({
+        id: true,
+        createdAt: true,
+        updatedAt: true,
+      }),
+    },
+    onRequest: verifyAuth([
+      AuthMode.JWT,
+      AuthMode.API_KEY,
+      AuthMode.SERVICE_TOKEN,
+      AuthMode.IDENTITY_ACCESS_TOKEN
+    ]),
+    handler: async (req) => {
+      return server.services.consumerSecret.create(req.body);
+    },
+  });
 
   // server.route({
   //   method: "PUT",
@@ -120,4 +126,44 @@ export const registerConsumerSecretRouter = async (server: FastifyZodProvider) =
   //     return server.services.consumerSecret.update();
   //   },
   // });
+
+  server.route({
+    method: "DELETE",
+    url: "/:consumerSecretId",
+    config: {
+      rateLimit: readLimit,
+    },
+    schema: {
+      description: "Delete consumer secret",
+      params: z.object({
+        consumerSecretId: z.string().trim()
+      }),
+      security: [
+        {
+          bearerAuth: []
+        }
+      ],
+      response: {
+        201: z.string(),
+      },
+    },
+    onRequest: verifyAuth([
+      AuthMode.JWT,
+      AuthMode.API_KEY,
+      AuthMode.SERVICE_TOKEN,
+      AuthMode.IDENTITY_ACCESS_TOKEN
+    ]),
+    handler: async (req) => {
+      const consumerSecret = await server.services.consumerSecret.get(req.params.consumerSecretId);
+      if (!consumerSecret) {
+        throw new NotFoundError({
+          message: "Consumer secret not found"
+        });
+      }
+
+      await server.services.consumerSecret.delete(req.params.consumerSecretId);
+
+      return;
+    },
+  });
 }
